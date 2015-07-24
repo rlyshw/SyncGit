1f6c894c8e2a8e3639678b19ee605c05a128a6ad
rlyshw <rlyshw@gmail.com>
2015-07-23 22:28:55 -0400

    formatting

--- gitsync.js
+++ gitsync.js
@@ -49,7 +49,7 @@ http.createServer(function(req, res) {
     res.end('no such location');
   });
 }).listen(port, '0.0.0.0');
-console.log("\nListening on port",port,"at",route,"for PUSH events on the",branch,"branch...");
+console.log("\nListening on port",port,"at",route,"for PUSH events on the",branch,"branch...\n");
 handler.on('error', function(err){
   console.error("Error:", err.message);
 });
